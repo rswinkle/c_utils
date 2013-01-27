7c6cf38c73e16633ecb1521b06598fdd6b7f7d0b
Robert Winkler <robert@robert-desktop.(none)>
2013-01-27 01:07:27 -0700

    added Premake etc

---
+++ premake4.lua
@@ -0,0 +1,44 @@
+
+-- A solution contains projects, and defines the available configurations
+solution "CUtils"
+   configurations { "Debug", "Release" }
+   location "build"
+ 
+
+   -- A project defines one build target
+   project "driver"
+      location "build"
+      kind "ConsoleApp"
+      language "C"
+      files { "./src/main.c" }
+ --     excludes { "c_utils.*" }
+ --     libdirs { "/usr/lib64/" }
+      includedirs { "./inc" }
+      links { "c_utils" } 
+	  targetdir "build"
+  
+      configuration "Debug"
+         defines { "DEBUG" }
+         flags { "Symbols" }
+ 
+      configuration "Release"
+         defines { "NDEBUG" }
+         flags { "Optimize" }
+ 
+      configuration { "linux", "gmake" }
+         buildoptions { "-ansi", "-pedantic-errors }
+
+   project "c_utils"
+      kind "SharedLib"
+      language "C"
+      files { "./src/**.c" }
+      excludes { "./src/main.c" }
+      includedirs { "./inc" }
+
+   
+   project "c_utils"
+      kind "StaticLib"
+      language "C"
+      files { "./src/**.c" }
+      excludes { "./src/main.c" }
+      includedirs { "./inc" }
